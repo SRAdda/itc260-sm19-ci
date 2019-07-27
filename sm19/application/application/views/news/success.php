1b0b50338a73045f6d1dae0b3c60efbacf1ac907
Sally Ross <46608728+SRAdda@users.noreply.github.com>
2019-07-27 12:38:13 -0700

    Creates patch to view issues
    program not rendering css

---
+++ sm19/application/application/views/news/success.php
@@ -0,0 +1,10 @@
+<?php
+// application/views/news/success.php
+$this->load->view($this->config->item('theme') . 'header');
+?>
+<h1>Victory!</h1>
+<p>Mission Accomplished.</p>
+
+<?php
+$this->load->view($this->config->item('theme') . 'footer');
+?>
